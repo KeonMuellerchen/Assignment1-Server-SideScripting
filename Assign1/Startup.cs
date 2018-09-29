1a5000d3f33bf4411ae3738997c96d70663a2811
Keon Muellerchen <Keonmuellerchen@gmail.com>
2018-09-29 15:23:26 -0400

    Add project files.

---
+++ Assign1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assign1.Startup))]
+namespace Assign1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
